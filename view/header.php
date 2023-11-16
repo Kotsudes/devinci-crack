587a5b0f8c5c193246e2ddfcf8e64f5617c82174
Kotsudes <ridge.lowao@edu.devinci.fr>
2023-11-16 12:39:52 +0100

    Ajout de la page XSS et fix du header

--- view/header.php
+++ view/header.php
@@ -12,7 +12,9 @@
   <body>
     <header>
       <div class="header">
-        <spam class="logo">Devinci Cracks</spam>
+        <a href="/devinci-cracks/routeur.php?controller=home&action=home&secure=true">
+          <spam class="logo">Devinci Cracks</spam>
+        </a>
         <a href=<?php echo ($secure == "true") ? "/devinci-cracks/routeur.php?controller={$_GET['controller']}&action={$_GET['action']}&secure=false" : "/devinci-cracks/routeur.php?controller={$_GET['controller']}&action={$_GET['action']}&secure=true"; ?>>
           <img class="lock" src=<?php echo ($secure == "true")? "/devinci-cracks/assets/lock.svg" : "/devinci-cracks/assets/unlock.svg"; ?> alt="logo" />
         </a>
--- view/xss.php
+++ view/xss.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>XSS Simulation Page</title>
+    <link rel="stylesheet" href="/devinci-cracks/style/xss.css" />
+
 </head>
 <body>
 
-<form method="post" action=<?php echo ($secure=="true")? "/devinci-cracks/xss/secure": "/devinci-cracks/xss/unsecure" ?>>
-    <label  for="username">Username:</label>
-    <input type="text" id="username" name="username">
-    <input type="submit" value="Submit">
-</form>
+<div class="xss">
+    <span>Attaque XSS</span>
+    <form method="post" action=<?php echo ($secure=="true")? "/devinci-cracks/xss/secure": "/devinci-cracks/xss/unsecure" ?>>
+        <label  for="username">Username:</label>
+        <input type="text" id="username" name="username">
+        <input type="submit" value="Submit">
+    </form>
+</div>
 
 </body>
 </html>
